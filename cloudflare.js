f3e838f21c06ad8744632667cb77cd40bdbc497a
newtbot <limtiseng@gmail.com>
2026-03-01 03:19:03 +0800

    updated for memo

--- cloudflare.js
+++ cloudflare.js
@@ -9,24 +9,63 @@ require("dotenv").config();
 
 class CloudFlare {
 	constructor(config) {
-		this.config = config
+		this.config = config;
 	}
 	//write the logic to comapre both public ip if one is different or blah update accordingly
 	async compareIP(domainName) {
 		try {
 			// console.log(await this.getIP())
 			// console.log(await this.getDNS(domainName))
-			if (await this.getIP() !== await this.getDNS(domainName)) {
+			if ((await this.getIP()) !== (await this.getDNS(domainName))) {
 				// console.log(domainName)
 				//call updateDNS
 				const newIp = await this.getIP();
-				this.updateDNS(newIp , domainName);
+				this.updateDNS(newIp, domainName);
 			}
-
 		} catch (error) {
 			console.log(error);
 		}
 	}
+
+	// async getAllDNS(subDomain) {
+	//     try {
+	//         // Access config based on the subdomain (e.g., 'memos' or 'proxy')
+	//         const domainConfig = config.domains[subDomain];
+
+	//         const options = {
+	//             method: 'GET',
+	//             // Cloudflare API list records endpoint
+	//             url: `${domainConfig.baseUrl}/${domainConfig.zoneId}/dns_records`,
+	//             headers: {
+	//                 'Content-Type': 'application/json',
+	//                 // Using the shared API token from your config
+	//                 'Authorization': "Bearer " + config.domains.apiToken
+	//             }
+	//         };
+
+	//         let cfRes = await axios.request(options);
+
+	//         // This will print all records. Look for the 'id' field
+	//         // to find your 'cloudflareDnsId' for the config.
+	//         console.log(`--- DNS Records for ${subDomain} ---`);
+	//         console.table(cfRes.data.result.map(r => ({
+	//             id: r.id,
+	//             name: r.name,
+	//             type: r.type,
+	//             content: r.content
+	//         })));
+
+	//         return cfRes.data.result;
+
+	//     } catch (error) {
+	//         // Improved error logging to see exactly what Cloudflare says
+	//         if (error.response) {
+	//             console.error("Cloudflare Error:", error.response.data);
+	//         } else {
+	//             console.error("Error:", error.message);
+	//         }
+	//     }
+	// }
 
 	//Use as helper by compareIp()
 	async getIP() {
@@ -41,7 +80,7 @@ class CloudFlare {
 
 	async getDNS(domainName) {
 		try {
-			const subDomain = domainName.split('.')[0];
+			const subDomain = domainName.split(".")[0];
 			const options = {
 				method: "GET",
 				url: `${config.domains[subDomain].baseUrl}/${config.domains[subDomain].zoneId}/dns_records/${config.domains[subDomain].cloudflareDnsId}`,
@@ -59,13 +98,13 @@ class CloudFlare {
 	}
 
 	//the logic will be called in compareIP
-	async updateDNS(iP , domainName) {
+	async updateDNS(iP, domainName) {
 		try {
 			//required!
 			// content
 			// name
 			// type
-			const subDomain = domainName.split('.')[0];
+			const subDomain = domainName.split(".")[0];
 			const utcTime = new Date().toISOString();
 
 			const options = {
@@ -97,40 +136,39 @@ if (require.main === module) {
 		try {
 			//chatgpt code ....
 			const domainValues = Object.values(config.domains)
-			.filter(obj => typeof obj === 'object' && obj.domain) 
-			.map(obj => obj.domain);
+				.filter((obj) => typeof obj === "object" && obj.domain)
+				.map((obj) => obj.domain);
 
-			//for each domain name in my base.js file. 
+			//for each domain name in my base.js file.
 			for (let domainName of domainValues) {
 				//write a logic to compare ip for domain in the loop
 				let cloudflare = new CloudFlare(config);
-				cloudflare.compareIP(domainName)
-
+				cloudflare.compareIP(domainName);
+				// cloudflare.getAllDNS("memos")
 			}
-
 		} catch (error) {
 			console.log("IIFE Error:", error);
 		}
 	})();
 }
 
-	//To get DNS RECORD ID.
-	// async getAllDNS(){
+// To get DNS RECORD ID.
+// async getAllDNS(){
 
-	//     try {
-	//         const options = {
-	//         method: 'GET',
-	//         url: `${this.baseUrl}${this.zoneId}/dns_records`,
-	//         headers: {
-	//                 'Content-Type': 'application/json',
-	//                 'Authorization': "Bearer " + this.apiToken
-	//             }
-	//         };
-	//         let cfRes = await axios.request(options)
-	//         console.log(cfRes.data.result)
+//     try {
+//         const options = {
+//         method: 'GET',
+//         url: `${this.baseUrl}${this.zoneId}/dns_records`,
+//         headers: {
+//                 'Content-Type': 'application/json',
+//                 'Authorization': "Bearer " + this.apiToken
+//             }
+//         };
+//         let cfRes = await axios.request(options)
+//         console.log(cfRes.data.result)
 
-	//     } catch (error) {
-	//         console.log(error.response); // this is the main part. Use the response property from the error object
-	//     }
+//     } catch (error) {
+//         console.log(error.response); // this is the main part. Use the response property from the error object
+//     }
 
-	// }
+// }
--- conf/base.js
+++ conf/base.js
@@ -1,24 +1,27 @@
 module.exports = {
-    domains: {
-            wireguard: {
-                domain: 'wireguard.teeseng.uk',
-                cloudflareDnsId: "ea93b6859cff64c4574a54912897a1f1",
-                zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
-                baseUrl: "https://api.cloudflare.com/client/v4/zones/",
-                apiToken: '__secrets.js__'
-            },
-            
-            proxy: {
-                domain: 'proxy.teeseng.uk',
-                cloudflareDnsId: "149403c78d119aecdfcde0bc97998b8e",
-                zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
-                baseUrl: "https://api.cloudflare.com/client/v4/zones/",
-                apiToken: '__secrets.js__'
+	domains: {
+		wireguard: {
+			domain: "wireguard.teeseng.uk",
+			cloudflareDnsId: "ea93b6859cff64c4574a54912897a1f1",
+			zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
+			baseUrl: "https://api.cloudflare.com/client/v4/zones/",
+			apiToken: "__secrets.js__",
+		},
 
-            },
+		proxy: {
+			domain: "proxy.teeseng.uk",
+			cloudflareDnsId: "149403c78d119aecdfcde0bc97998b8e",
+			zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
+			baseUrl: "https://api.cloudflare.com/client/v4/zones/",
+			apiToken: "__secrets.js__",
+		},
 
-        }
-}
-
-
-
+		memos: {
+			domain: "memos.teeseng.uk",
+			cloudflareDnsId: "0d070c6815c0e991cbab41d21a2e3e5b",
+			zoneId: "11f8ec3c2ff0530fe6f1b97dfe4b8f74",
+			baseUrl: "https://api.cloudflare.com/client/v4/zones/",
+			apiToken: "__secrets.js__",
+		},
+	},
+};
